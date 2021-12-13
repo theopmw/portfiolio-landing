f4fef6377f7b6bbd11acf43194c28c38bca2c26d
Theo Wright <theowrightdev@gmail.com>
2021-12-13 20:04:47 +0000

    Add heading and gsap text hover functionality

---
+++ static/js/title.js
@@ -0,0 +1,15 @@
+document.body.addEventListener("mousemove", event => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    gsap.set(".cursor", {
+        x: mouseX,
+        y: mouseY, 
+    })
+
+    gsap.to(".shape", {
+        x: mouseX,
+        y: mouseY,
+        stagger: -0.1
+    })
+});
