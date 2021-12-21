77e25024007608cb67fe820b0f7ab5942a8089e5
Theo Wright <theowrightdev@gmail.com>
2021-12-21 11:12:57 +0000

    Add CSS and JS to navbar

--- static/js/title.js
+++ static/js/title.js
@@ -13,3 +13,10 @@ document.getElementById("hero").addEventListener("mousemove", event => {
         stagger: -0.1
     })
 });
+
+
+// mobile menu slide from the left
+$('[data-toggle="slide-collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.animate({'width':'toggle'}, 350);
+});
